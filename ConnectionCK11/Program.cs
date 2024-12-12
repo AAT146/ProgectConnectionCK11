e67364bffe5c7da10c202ae02b55513eac4cba1b
Tupikova Anastasiya <tupikova.a.00@yandex.ru>
2024-12-13 00:00:08 +0700

    fix():

--- ConnectionCK11/Program.cs
+++ ConnectionCK11/Program.cs
@@ -12,38 +12,60 @@ using System.Security.Cryptography;
 
 namespace WriteRequestCK11
 {
+	// Для обращения к большинству методов API СК-11 требуется, чтобы запрос был аутентифицирован. 
+	// Аутентификация в запросах к API СК-11, осуществляется путем передачи токена доступа в виде Bearer-токена в заголовке каждого запроса и 
+	// предназначена для аутентификации запросов из внешних серверных приложений. 
+	// Токен доступа — это непрозрачная строка, которая соответствует идентификатору сессии аутентификации.
 	class Token
 	{
+		// Токен доступа
 		public string access_token { get; set; }
+		// Признак, обозначающий, что данный идентификатор может быть использовать со схемой аутентификации Bearer
 		public string token_type { get; set; }
+		// Количество секунд, через которое время жизни сессии истечет
 		public string expires_in { get; set; }
+		// Логин пользователя
 		public string user_login { get; set; }
+		// Имя компьютера, с которого произведен вход (получен активный токен доступа), либо IP адресс, если получить имя не удалось
 		public string user_host { get; set; }
 	}
 
+	// Внутренняя схема тела запроса (для формирования записи данных)
 	class Value
 	{
+		// UID значения измерений
 		public string uid { get; set; }
+		// Левая граница интервала
 		public string timeStamp { get; set; }
+		// Правая граница интервала
 		public string timeStamp2 { get; set; }
+		// Код качества
 		public long qCode { get; set; }
+		// Значение измерения
 		public double value { get; set; }
 	}
 
+	// Схема тела запроса
 	class WriteRequest
 	{
+		// Тип записи данных
 		public string writeType { get; set; }
+		// Лист значений измерений
 		public List<Value> values { get; set; }
 	}
 
 	class Program
 	{
+		// Определение пользовательских параметров приложения: строки соединения с БД или заголовки окна браузера
+		// Один элемент - add, два атрибута: key и value
 		static string ck11PolEP = ConfigurationManager.AppSettings["ck11EndPoint"];
 		static string auth = ck11PolEP + ConfigurationManager.AppSettings["ck11TokenEndPoint"];
 		static string measurWrite = ck11PolEP + ConfigurationManager.AppSettings["ck11MeasurWriteEndPoint"];
 
+		// Получить токен доступа 
 		static Token getToken()
 		{
+			// Запрос токена
 			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3;
 			Console.WriteLine("Запрос токена в СК11");
 			WebRequest webRequestToken = WebRequest.Create(auth);
@@ -61,14 +83,17 @@ namespace WriteRequestCK11
 			return JsonConvert.DeserializeObject<Token>(tokenBody);
 		}
 
+		// Записать данные в СК-11
 		public static void writeDataToCK11(string uid, string timeStamp, string timeStamp2, long qCode, double value)
 		{
 			ServicePointManager.Expect100Continue = true;
 			ServicePointManager.DefaultConnectionLimit = 9999;
 			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3;
 
+			// Получение токена
 			Token token = getToken();
 
+			// Запись значений
 			Console.WriteLine("Запись данных в СК11");
 			WebRequest webRequestWrite = WebRequest.Create(measurWrite);
 			webRequestWrite.Method = "POST";
@@ -76,6 +101,7 @@ namespace WriteRequestCK11
 			webRequestWrite.Headers.Add("charset", "UTF-8");
 			webRequestWrite.Headers.Add("Authorization", token.token_type + " " + token.access_token);
 
+			// Тело запроса на запись
 			WriteRequest bodyRequest = new WriteRequest
 			{
 				/*writeType = "forcedToArchive",*/
